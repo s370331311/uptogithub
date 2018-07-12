b50e19695c27b28ea693445e263e5c27b06ea5fd
s370331311 <fys.shiyufeng@163.com>
2018-07-12 19:56:48 +0800

    test

--- main.ts
+++ main.ts
@@ -2,4 +2,493 @@
 //% color="#006400" weight=20 icon="\uf1b9"
 namespace cbit_motor {
 
+    const PCA9685_ADD = 0x41
+    const MODE1 = 0x00
+    const MODE2 = 0x01
+    const SUBADR1 = 0x02
+    const SUBADR2 = 0x03
+    const SUBADR3 = 0x04
+
+    const LED0_ON_L = 0x06
+    const LED0_ON_H = 0x07
+    const LED0_OFF_L = 0x08
+    const LED0_OFF_H = 0x09
+
+    const ALL_LED_ON_L = 0xFA
+    const ALL_LED_ON_H = 0xFB
+    const ALL_LED_OFF_L = 0xFC
+    const ALL_LED_OFF_H = 0xFD
+
+    const PRESCALE = 0xFE
+
+
+    export enum enPos {
+
+        //% blockId="LeftState" block="左边状态"
+        LeftState = 1,
+        //% blockId="RightState" block="右边状态"
+        RightState = 0
+    }
+
+    export enum enLineState {
+        //% blockId="White" block="白线"
+        White = 0,
+        //% blockId="Black" block="黑线"
+        Black = 1
+
+    }
+    
+    export enum enAvoidState {
+        //% blockId="OBSTACLE" block="有障碍物"
+        OBSTACLE = 0,
+        //% blockId="NOOBSTACLE" block="无障碍物"
+        NOOBSTACLE = 1
+    }
+
+    
+    export enum enServo {
+        S1 = 1,
+        S2,
+        S3
+    }
+    export enum CarState {
+        //% blockId="Car_Run" block="前行"
+        Car_Run = 1,
+        //% blockId="Car_Back" block="后退"
+        Car_Back = 2,
+        //% blockId="Car_Left" block="左转"
+        Car_Left = 3,
+        //% blockId="Car_Right" block="右转"
+        Car_Right = 4,
+        //% blockId="Car_Stop" block="停止"
+        Car_Stop = 5,
+        //% blockId="Car_SpinLeft" block="原地左旋"
+        Car_SpinLeft = 6,
+        //% blockId="Car_SpinRight" block="原地右旋"
+        Car_SpinRight = 7         
+    }
+    export enum AloneState {
+        //% blockId="Right_Z_Motor" block="右侧电机正转"
+        Right_Z_Motor = 1,
+        //% blockId="Right_F_Motor" block="右侧电机反转"
+        Right_F_Motor = 2,
+        //% blockId="Left_Z_Motor" block="左侧电机正转"
+        Left_Z_Motor = 3,
+        //% blockId="Left_F_Motor" block="左侧电机反转"
+        Left_F_Motor = 4       
+    }
+
+    function i2cwrite(addr: number, reg: number, value: number) {
+        let buf = pins.createBuffer(2)
+        buf[0] = reg
+        buf[1] = value
+        pins.i2cWriteBuffer(addr, buf)
+    }
+
+    function i2ccmd(addr: number, value: number) {
+        let buf = pins.createBuffer(1)
+        buf[0] = value
+        pins.i2cWriteBuffer(addr, buf)
+    }
+
+    function i2cread(addr: number, reg: number) {
+        pins.i2cWriteNumber(addr, reg, NumberFormat.UInt8BE);
+        let val = pins.i2cReadNumber(addr, NumberFormat.UInt8BE);
+        return val;
+    }
+
+    function initPCA9685(): void {
+        i2cwrite(PCA9685_ADD, MODE1, 0x00)
+        setFreq(50);
+        initialized = true
+    }
+
+    function setFreq(freq: number): void {
+        // Constrain the frequency
+        let prescaleval = 25000000;
+        prescaleval /= 4096;
+        prescaleval /= freq;
+        prescaleval -= 1;
+        let prescale = prescaleval; //Math.Floor(prescaleval + 0.5);
+        let oldmode = i2cread(PCA9685_ADD, MODE1);
+        let newmode = (oldmode & 0x7F) | 0x10; // sleep
+        i2cwrite(PCA9685_ADD, MODE1, newmode); // go to sleep
+        i2cwrite(PCA9685_ADD, PRESCALE, prescale); // set the prescaler
+        i2cwrite(PCA9685_ADD, MODE1, oldmode);
+        control.waitMicros(5000);
+        i2cwrite(PCA9685_ADD, MODE1, oldmode | 0xa1);
+    }
+
+    function setPwm(channel: number, on: number, off: number): void {
+        if (channel < 0 || channel > 15)
+            return;
+        if (!initialized) {
+            initPCA9685();
+        }
+        let buf = pins.createBuffer(5);
+        buf[0] = LED0_ON_L + 4 * channel;
+        buf[1] = on & 0xff;
+        buf[2] = (on >> 8) & 0xff;
+        buf[3] = off & 0xff;
+        buf[4] = (off >> 8) & 0xff;
+        pins.i2cWriteBuffer(PCA9685_ADD, buf);
+    }
+
+
+    function Car_run(speed: number) {
+
+        speed = speed * 16; // map 350 to 4096
+        if (speed >= 4096) {
+            speed = 4095
+        }
+        if (speed <= 350) {
+            speed = 350
+        }
+
+        setPwm(12, 0, speed);
+        setPwm(13, 0, 0);
+
+        setPwm(15, 0, speed);
+        setPwm(14, 0, 0);
+        //pins.digitalWritePin(DigitalPin.P16, 1);
+       // pins.analogWritePin(AnalogPin.P1, 1023-speed); //速度控制
+
+       // pins.analogWritePin(AnalogPin.P0, speed);//速度控制
+       // pins.digitalWritePin(DigitalPin.P8, 0);
+    }
+   function Left_Z_run(speed: number) {
+
+        speed = speed * 16; // map 350 to 4096
+        if (speed >= 4096) {
+            speed = 4095
+        }
+        if (speed <= 350) {
+            speed = 350
+        }
+
+        setPwm(12, 0, speed);
+        setPwm(13, 0, 0);
+
+        //setPwm(15, 0, 0);
+        //setPwm(14, 0, 0);
+    }
+   function Left_F_run(speed: number) {
+
+        speed = speed * 16; // map 350 to 4096
+        if (speed >= 4096) {
+            speed = 4095
+        }
+        if (speed <= 350) {
+            speed = 350
+        }
+
+        setPwm(12, 0, 0);
+        setPwm(13, 0, speed);
+
+        //setPwm(15, 0, 0);
+        //setPwm(14, 0, 0);
+    }    
+     function Right_Z_run(speed: number) {
+
+        speed = speed * 16; // map 350 to 4096
+        if (speed >= 4096) {
+            speed = 4095
+        }
+        if (speed <= 350) {
+            speed = 350
+        }
+
+       // setPwm(12, 0, 0);
+       // setPwm(13, 0, 0);
+
+        setPwm(15, 0, speed);
+        setPwm(14, 0, 0);
+    }
+     function Right_F_run(speed: number) {
+
+        speed = speed * 16; // map 350 to 4096
+        if (speed >= 4096) {
+            speed = 4095
+        }
+        if (speed <= 350) {
+            speed = 350
+        }
+
+       // setPwm(12, 0, 0);
+       // setPwm(13, 0, 0);
+
+        setPwm(15, 0, 0);
+        setPwm(14, 0, speed);
+    }    
+    function Car_back(speed: number) {
+
+        speed = speed * 16; // map 350 to 4096
+        if (speed >= 4096) {
+            speed = 4095
+        }
+        if (speed <= 350 && speed != 0) {
+            speed = 350
+        }
+
+        setPwm(12, 0, 0);
+        setPwm(13, 0, speed);
+
+        setPwm(15, 0, 0);
+        setPwm(14, 0, speed);
+
+        //pins.digitalWritePin(DigitalPin.P16, 0);
+        //pins.analogWritePin(AnalogPin.P1, speed); //速度控制
+
+        //pins.analogWritePin(AnalogPin.P0, 1023 - speed);//速度控制
+        //pins.digitalWritePin(DigitalPin.P8, 1);
+    }
+
+    function Car_left(speed: number) {
+
+        speed = speed * 16; // map 350 to 4096
+        if (speed >= 4096) {
+            speed = 4095
+        }
+        if (speed <= 350 && speed != 0) {
+            speed = 350
+        }
+        setPwm(12, 0, 0);
+        setPwm(13, 0, 0);
+
+        setPwm(15, 0, speed);
+        setPwm(14, 0, 0);
+
+        //pins.analogWritePin(AnalogPin.P0, speed);
+        //pins.digitalWritePin(DigitalPin.P8, 0);
+
+        //pins.digitalWritePin(DigitalPin.P16, 0);
+        //pins.digitalWritePin(DigitalPin.P1, 0);
+    }
+
+    function Car_right(speed: number) {
+
+        speed = speed * 16; // map 350 to 4096
+        if (speed >= 4096) {
+            speed = 4095
+        }
+        if (speed <= 350 && speed != 0) {
+            speed = 350
+        }
+        setPwm(12, 0, speed);
+        setPwm(13, 0, 0);
+
+        setPwm(15, 0, 0);
+        setPwm(14, 0, 0);
+        //pins.digitalWritePin(DigitalPin.P0, 0);
+        //pins.digitalWritePin(DigitalPin.P8, 0);
+
+        //pins.digitalWritePin(DigitalPin.P16, 1);
+       // pins.analogWritePin(AnalogPin.P1, 1023 - speed);
+    }
+
+    function Car_stop() {
+       
+        setPwm(12, 0, 0);
+        setPwm(13, 0, 0);
+
+        setPwm(15, 0, 0);
+        setPwm(14, 0, 0);
+        //pins.digitalWritePin(DigitalPin.P0, 0);
+        //pins.digitalWritePin(DigitalPin.P8, 0);
+        //pins.digitalWritePin(DigitalPin.P16, 0);
+        //pins.digitalWritePin(DigitalPin.P1, 0);
+    }
+
+    function Car_spinleft(speed: number) {
+
+        speed = speed * 16; // map 350 to 4096
+        if (speed >= 4096) {
+            speed = 4095
+        }
+        if (speed <= 350 && speed != 0) {
+            speed = 350
+        }
+        setPwm(12, 0, 0);
+        setPwm(13, 0, speed);
+
+        setPwm(15, 0, speed);
+        setPwm(14, 0, 0);
+
+        //pins.analogWritePin(AnalogPin.P0, speed);
+        //pins.digitalWritePin(DigitalPin.P8, 0);
+
+        //pins.digitalWritePin(DigitalPin.P16, 0);
+        //pins.analogWritePin(AnalogPin.P1, speed);
+    } 
+
+    function Car_spinright(speed: number) {
+
+        speed = speed * 16; // map 350 to 4096
+        if (speed >= 4096) {
+            speed = 4095
+        }
+        if (speed <= 350 && speed != 0) {
+            speed = 350
+        }
+        setPwm(12, 0, speed);
+        setPwm(13, 0, 0);
+
+        setPwm(15, 0, 0);
+        setPwm(14, 0, speed);
+        //pins.analogWritePin(AnalogPin.P0, 1023-speed);
+        //pins.digitalWritePin(DigitalPin.P8, 1);
+
+        //pins.digitalWritePin(DigitalPin.P16, 1);
+        //pins.analogWritePin(AnalogPin.P1, 1023-speed);
+
+    }
+
+    //% blockId=cbit_Servo_Car block="小车舵机|编号 %num|角度 %value"
+    //% weight=96
+    //% blockGap=10
+    //% color="#006400"
+    //% num.min=1 num.max=3 value.min=0 value.max=180
+    //% name.fieldEditor="gridpicker" name.fieldOptions.columns=9
+    export function Servo_Car(num: enServo, value: number): void {
+
+        // 50hz: 20,000 us
+        let us = (value * 1800 / 180 + 600); // 0.6 ~ 2.4
+        let pwm = us * 4096 / 20000;
+        setPwm(num + 2, 0, pwm);
+
+    }
+
+    //% blockId=cbit_Avoid_Sensor block="避障传感器|检测到 %value"
+    //% weight=95
+    //% blockGap=10
+    //% color="#006400"
+    //% name.fieldEditor="gridpicker" name.fieldOptions.columns=12
+    export function Avoid_Sensor(value: enAvoidState): boolean {
+
+        let temp: boolean = false;
+        pins.digitalWritePin(DigitalPin.P9, 0);
+        switch (value) {
+            case enAvoidState.OBSTACLE: {
+                if (pins.analogReadPin(AnalogPin.P3) < 800) {
+                
+                    temp = true;
+                    setPwm(8, 0, 0);
+                }
+                else {                 
+                    temp = false;
+                    setPwm(8, 0, 4095);
+                }
+                break;
+            }
+
+            case enAvoidState.NOOBSTACLE: {
+                if (pins.analogReadPin(AnalogPin.P3) > 800) {
+
+                    temp = true;
+                    setPwm(8, 0, 4095);
+                }
+                else {
+                    temp = false;
+                    setPwm(8, 0, 0);
+                }
+                break;
+            }
+        }
+        pins.digitalWritePin(DigitalPin.P9, 1);
+        return temp;
+
+    }
+    //% blockId=cbit_Line_Sensor1 block="巡线传感器1|位置 %direct|检测到 %value"
+    //% weight=94
+    //% blockGap=10
+    //% color="#006400"
+    //% name.fieldEditor="gridpicker" name.fieldOptions.columns=12
+    export function Line_Sensor1(direct: enPos, value: enLineState): boolean {
+
+        let temp: boolean = false;
+
+        switch (direct) {
+            case enPos.LeftState: {
+                if (pins.analogReadPin(AnalogPin.P1) < 500) {
+                    if (value == enLineState.White) {
+                        temp = true;
+                    }
+                    setPwm(7, 0, 4095);
+                }
+                else {
+                    if (value == enLineState.Black) {
+                        temp = true;
+                    }
+                    setPwm(7, 0, 0);
+                }
+                break;
+            }
+
+            case enPos.RightState: {
+                if (pins.analogReadPin(AnalogPin.P2) < 500) {
+                    if (value == enLineState.White) {
+                        temp = true;
+                    }
+                    setPwm(6, 0, 4095);
+                }
+                else {
+                    if (value == enLineState.Black) {
+                        temp = true;
+                    }
+                    setPwm(6, 0, 0);
+                }
+                break;
+            }
+        }
+        return temp;
+
+    }
+
+    //% blockId=cbit_CarCtrl block="小车控制|%index"
+    //% weight=93
+    //% blockGap=10
+    //% color="#006400"
+    //% name.fieldEditor="gridpicker" name.fieldOptions.columns=10
+    export function CarCtrl(index: CarState): void {
+        switch (index) {
+            case CarState.Car_Run: Car_run(255); break;
+            case CarState.Car_Back: Car_back(255); break;
+            case CarState.Car_Left: Car_left(255); break;
+            case CarState.Car_Right: Car_right(255); break;
+            case CarState.Car_Stop: Car_stop(); break;
+            case CarState.Car_SpinLeft: Car_spinleft(255); break;
+            case CarState.Car_SpinRight: Car_spinright(255); break;
+        }
+    }
+    //% blockId=cbit_CarCtrlSpeed block="小车控制|%index|速度 %speed"
+    //% weight=92
+    //% blockGap=10
+    //% speed.min=0 speed.max=255
+    //% color="#006400"
+    //% name.fieldEditor="gridpicker" name.fieldOptions.columns=10
+    export function CarCtrlSpeed(index: CarState, speed: number): void {
+        switch (index) {
+            case CarState.Car_Run: Car_run(speed); break;
+            case CarState.Car_Back: Car_back(speed); break;
+            case CarState.Car_Left: Car_left(speed); break;
+            case CarState.Car_Right: Car_right(speed); break;
+            case CarState.Car_Stop: Car_stop(); break;
+            case CarState.Car_SpinLeft: Car_spinleft(speed); break;
+            case CarState.Car_SpinRight: Car_spinright(speed); break;
+        }
+    }
+    //% blockId=cbit_AloneCtrlSpeed block="单独电机|%index|速度 %speed"
+    //% weight=91
+    //% blockGap=10
+    //% speed.min=0 speed.max=255
+    //% color="#006400"
+    //% name.fieldEditor="gridpicker" name.fieldOptions.columns=10
+    export function AloneCtrlSpeed(index: AloneState, speed: number): void {
+        switch (index) {
+            case AloneState.Right_Z_Motor: Right_Z_run(speed); break;
+            case AloneState.Right_F_Motor: Right_F_run(speed); break;
+            case AloneState.Left_Z_Motor: Left_Z_run(speed); break;
+            case AloneState.Left_F_Motor: Left_F_run(speed); break;
+        }
+    }    
 }
