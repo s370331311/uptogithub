497dce76025126a48b20370bd396c3e7295fe2b6
s370331311 <fys.shiyufeng@163.com>
2018-07-12 15:49:19 +0800

    car_motor

--- main.ts
+++ main.ts
@@ -6,429 +6,8 @@ load dependency
 */
 
 
-
-//% color="#C814B8" weight=25 icon="\uf1d4"
-namespace cbit_显示类 {
-    
-    export enum enColor {
-
-        //% blockId="OFF" block="灭"
-        OFF = 0,
-        //% blockId="Red" block="红色"
-        Red,
-        //% blockId="Green" block="绿色"
-        Green,
-        //% blockId="Blue" block="蓝色"
-        Blue,
-        //% blockId="White" block="白色"
-        White,
-        //% blockId="Cyan" block="青色"
-        Cyan,
-        //% blockId="Pinkish" block="品红"
-        Pinkish,
-        //% blockId="Green" block="黄"
-        Yellow,
-
-    }
-    export enum enLED1 {
-        
-        //% blockId="OFF" block="灭"
-        OFF = 0,
-        //% blockId="ON" block="亮"
-        ON =1
-    }
-
-    //% blockId=cbit_LED1 block="LED灯|引脚 %pin|状态 %value"
-    //% weight=5
-    //% blockGap=8
-    //% color="#C814B8"
-    //% name.fieldEditor="gridpicker" name.fieldOptions.columns=1
-    export function LED1(pin: DigitalPin, value: enLED1): void {
-
-        pins.digitalWritePin(pin, value);
-
-    }
-
-    //% blockId=cbit_LED2 block="LED灯|引脚 %pin|亮度 %value"
-    //% weight=4
-    //% blockGap=8
-    //% color="#C814B8"
-    //% value.min=0 value.max=255
-    //% name.fieldEditor="gridpicker" name.fieldOptions.columns=2
-    export function LED2(pin: AnalogPin, value: number): void {
-
-        pins.analogWritePin(pin, value * 1024 / 256);
-
-    }
-
-    //% blockId=cbit_BreathLED block="呼吸灯|引脚 %pin"
-    //% weight=3
-    //% blockGap=8
-    //% color="#C814B8"
-    //% name.fieldEditor="gridpicker" name.fieldOptions.columns=3
-    export function BreathLED(pin: AnalogPin): void {
-
-        for (let i: number = 0; i < 1023; i++) {
-            pins.analogWritePin(pin, i);
-            //basic.pause(1);
-            control.waitMicros(1000);
-        }
-        basic.pause(10);
-        for (let i: number = 1023; i > 0; i--) {
-            pins.analogWritePin(pin, i);
-            //basic.pause(1);
-            control.waitMicros(1000);
-        }
-
-    }
-
-    //% blockId=cbit_RGB block="RGB七彩灯|引脚R %pin1|引脚G %pin2|引脚B %pin3|红色 %value1|绿色 %value2|蓝色 %value3"
-    //% weight=2
-    //% blockGap=8
-    //% color="#C814B8"
-    //% value1.min=0 value1.max=255 value2.min=0 value2.max=255 value3.min=0 value3.max=255
-    //% name.fieldEditor="gridpicker" name.fieldOptions.columns=4
-    export function RGB(pin1: AnalogPin, pin2: AnalogPin, pin3: AnalogPin, value1: number, value2: number, value3: number): void {
-
-        pins.analogWritePin(pin1, value1 * 1024 / 256);
-        pins.analogWritePin(pin2, value2 * 1024 / 256);
-        pins.analogWritePin(pin3, value3 * 1024 / 256);
-
-    }
-    //% blockId=cbit_RGB2 block="RGB七彩灯|引脚R %pin1|引脚G %pin2|引脚B %pin3|显示 %value"
-    //% weight=1
-    //% blockGap=8
-    //% color="#C814B8"
-    //% name.fieldEditor="gridpicker" name.fieldOptions.columns=4
-    export function RGB2(pin1: DigitalPin, pin2: DigitalPin, pin3: DigitalPin, value: enColor): void {
-
-        switch (value) {
-            case enColor.OFF: {
-                pins.digitalWritePin(pin1, 0);
-                pins.digitalWritePin(pin2, 0);
-                pins.digitalWritePin(pin3, 0);
-                break;
-            }
-            case enColor.Red: {
-                pins.digitalWritePin(pin1, 1);
-                pins.digitalWritePin(pin2, 0);
-                pins.digitalWritePin(pin3, 0);
-                break;
-            }
-            case enColor.Green: {
-                pins.digitalWritePin(pin1, 0);
-                pins.digitalWritePin(pin2, 1);
-                pins.digitalWritePin(pin3, 0);
-                break;
-            }
-            case enColor.Blue: {
-                pins.digitalWritePin(pin1, 0);
-                pins.digitalWritePin(pin2, 0);
-                pins.digitalWritePin(pin3, 1);
-                break;
-            }
-            case enColor.White: {
-                pins.digitalWritePin(pin1, 1);
-                pins.digitalWritePin(pin2, 1);
-                pins.digitalWritePin(pin3, 1);
-                break;
-            }
-            case enColor.Cyan: {
-                pins.digitalWritePin(pin1, 0);
-                pins.digitalWritePin(pin2, 1);
-                pins.digitalWritePin(pin3, 1);
-                break;
-            }
-            case enColor.Pinkish: {
-                pins.digitalWritePin(pin1, 1);
-                pins.digitalWritePin(pin2, 0);
-                pins.digitalWritePin(pin3, 1);
-                break;
-            }
-            case enColor.Yellow: {
-                pins.digitalWritePin(pin1, 1);
-                pins.digitalWritePin(pin2, 1);
-                pins.digitalWritePin(pin3, 0);
-                break;
-            }
-        }
-
-    }
-   
-}
-/*****************************************************************************************************************************************
- *  传感器类 ***************************************************************************************************************************** 
- ****************************************************************************************************************************************/
-
-//% color="#87CEEB" weight=24 icon="\uf1b6"
-namespace cbit_传感器类 {
-
-    export enum enVoice {
-        //% blockId="Voice" block="有声音"
-        Voice = 0,
-        //% blockId="NoVoice" block="无声音"
-        NoVoice = 1
-    }
-
-    export enum enIR {
-        //% blockId="Get" block="检测到"
-        Get = 0,
-        //% blockId="NoVoice" block="未检测"
-        NoGet = 1
-    }
-    
-
-    //% blockId=cbit_Voice_Sensor block="声音传感器|引脚 %pin|返回 %value"
-    //% weight=100
-    //% blockGap=10
-    //% color="#87CEEB"
-    //% name.fieldEditor="gridpicker" name.fieldOptions.columns=4
-    export function Voice_Sensor(pin: DigitalPin, value: enVoice): boolean {
-
-        pins.setPull(pin, PinPullMode.PullUp);
-        if (pins.digitalReadPin(pin) == value) {
-            return true;
-        }
-        else {
-            return false;
-        }
-
-    }
-
-    function IR_send_38k() {
-        for (let i: number = 0; i < 8; i++) {
-            pins.digitalWritePin(DigitalPin.P9, 1);
-            control.waitMicros(13);
-            pins.digitalWritePin(DigitalPin.P9, 0);
-            control.waitMicros(13);
-        }
-    }
-    //% blockId=cbit_IR_Sensor block="红外传感器|引脚 %pin|  |%value|障碍物"
-    //% weight=100
-    //% blockGap=10
-    //% color="#87CEEB"
-    //% name.fieldEditor="gridpicker" name.fieldOptions.columns=4
-    export function IR_Sensor(pin: DigitalPin, value: enIR): boolean {
-
-        pins.setPull(pin, PinPullMode.PullUp);
-        //IR_send_38k();
-        if (pins.digitalReadPin(pin) == value) {
-            return true;
-        }
-        else {
-            return false;
-        }
-
-    }
-
-    //% blockId=cbit_IR_Send block="红外发射|引脚 %pin"
-    //% weight=100
-    //% blockGap=10
-    //% color="#87CEEB"
-    //% name.fieldEditor="gridpicker" name.fieldOptions.columns=4
-    export function IR_Send(pin: DigitalPin): void {
-
-        
-        IR_send_38k();
-
-    }
-   
-    //% blockId=cbit_ultrasonic block="超声波|发射管脚 %Trig|接收管脚 %Echo"
-    //% color="#87CEEB"
-    //% weight=100
-    //% blockGap=10
-    //% name.fieldEditor="gridpicker" name.fieldOptions.columns=4
-    export function Ultrasonic(Trig: DigitalPin, Echo: DigitalPin): number {
-
-        // send pulse
-        pins.setPull(Trig, PinPullMode.PullNone);
-        pins.digitalWritePin(Trig, 0);
-        control.waitMicros(2);
-        pins.digitalWritePin(Trig, 1);
-        control.waitMicros(10);
-        pins.digitalWritePin(Trig, 0);
-
-        // read pulse
-        let d = pins.pulseIn(Echo, PulseValue.High, 23200);
-        return d / 58;
-    }
-}
-
-/*****************************************************************************************************************************************
- *  输入类 *****************************************************************************************************************************
- ****************************************************************************************************************************************/
-
-//% color="#808080" weight=23 icon="\uf11c"
-namespace cbit_输入类 {
-
-    export enum enRocker {
-        //% blockId="Nostate" block="无"
-        Nostate = 0,
-        //% blockId="Up" block="上"
-        Up,
-        //% blockId="Down" block="下"
-        Down,
-        //% blockId="Left" block="左"
-        Left,
-        //% blockId="Right" block="右"
-        Right,
-        //% blockId="Press" block="按下"
-        Press
-    }
-
-    export enum enTouch {
-        //% blockId="NoTouch" block="未触摸"
-        NoTouch = 0,
-        //% blockId="Touch" block="触摸"
-        Touch = 1
-    }
-    export enum enButton {
-        //% blockId="Press" block="按下"
-        Press = 0,
-        //% blockId="Realse" block="松开"
-        Realse = 1
-    }
-
-    //% blockId=cbit_TouchPad block="触摸开关|引脚 %pin|返回 %value"
-    //% weight=100
-    //% blockGap=10
-    //% color="#808080"
-    //% name.fieldEditor="gridpicker" name.fieldOptions.columns=5
-    export function TouchPad(pin: DigitalPin, value: enTouch): boolean {
-
-        pins.setPull(pin, PinPullMode.PullUp);
-        if (pins.digitalReadPin(pin) == value) {
-            return true;
-        }
-        else {
-            return false;
-        }
-
-    }
-    //% blockId=cbit_Rocker block="遥杆|VRX %pin1|VRY %pin2|SW %pin3|返回 %value"
-    //% weight=100
-    //% blockGap=10
-    //% color="#808080"
-    //% name.fieldEditor="gridpicker" name.fieldOptions.columns=6
-    export function Rocker(pin1: AnalogPin, pin2: AnalogPin, pin3: AnalogPin, value: enRocker): boolean {
-
-        //pins.setPull(pin3, PinPullMode.PullUp);
-        let x = pins.analogReadPin(pin1);
-        let y = pins.analogReadPin(pin2);
-        let z = pins.analogReadPin(pin3);
-        let now_state = enRocker.Nostate;
-
-        if (x <= 20) // 上
-        {
-
-            now_state = enRocker.Up;
-
-        }
-        if (x >= 1000) //
-        {
-
-            now_state = enRocker.Down;
-        }
-        if (y <= 50) //右
-        {
-            now_state = enRocker.Right;
-        }
-        if (y >= 1000) //左
-        {
-            now_state = enRocker.Left;
-        }
-        if (z <= 20)
-            now_state = enRocker.Press;
-        
-        if (now_state == value)
-            return true;
-        else
-            return false;
-
-    }
-
-    //% blockId=cbit_Button block="按键|引脚 %pin|返回 %value"
-    //% weight=100
-    //% blockGap=10
-    //% color="#808080"
-    //% name.fieldEditor="gridpicker" name.fieldOptions.columns=5
-    export function Button(pin: DigitalPin, value: enButton): boolean {
-
-        pins.setPull(pin, PinPullMode.PullUp);
-        if (pins.digitalReadPin(pin) == value) {
-            return true;
-        }
-        else {
-            return false;
-        }
-
-    }  
-}
-
-/*****************************************************************************************************************************************
- *    音乐类 *****************************************************************************************************************************
- ****************************************************************************************************************************************/
-
-//% color="#D2691E" weight=22 icon="\uf001"
-namespace cbit_音乐类 {
-    export enum enBuzzer {
-
-        //% blockId="NoBeep" block="响"
-        NoBeep = 0,
-        //% blockId="Beep" block="不响"
-        Beep
-    }
-
-    //% blockId=cbit_Buzzer block="有源蜂鸣器|引脚 %pin|值 %value"
-    //% weight=100
-    //% blockGap=10 
-    //% color="#D2691E"
-    //% value.min=0 value.max=1
-    //% name.fieldEditor="gridpicker" name.fieldOptions.columns=8
-    export function Buzzer(pin: DigitalPin, value: enBuzzer): void {
-
-        pins.setPull(pin, PinPullMode.PullNone);
-        pins.digitalWritePin(pin, value);
-
-    }
-
-}
-
-/*****************************************************************************************************************************************
- *    电机类 *****************************************************************************************************************************
- ****************************************************************************************************************************************/
-
-//% color="#0000CD" weight=21 icon="\uf185"
-namespace cbit_电机类 {
-
-    //% blockId=cbit_Fan block="风扇|引脚 %pin|速度 %value"
-    //% weight=100
-    //% blockGap=10
-    //% color="#0000CD"
-    //% value.min=0 value.max=1023
-    //% name.fieldEditor="gridpicker" name.fieldOptions.columns=9
-    export function Fan(pin: AnalogPin, value: number): void {
-
-        pins.analogWritePin(pin, value);
-
-    }
-
-    //% blockId=cbit_Servo block="舵机|引脚 %pin|角度 %value"
-    //% weight=100
-    //% blockGap=10
-    //% color="#0000CD"
-    //% value.min=0 value.max=180
-    //% name.fieldEditor="gridpicker" name.fieldOptions.columns=9
-    export function Servo(pin: AnalogPin, value: number): void {
-
-        pins.servoWritePin(pin, value);
-
-    }
-
-}
-
 //% color="#006400" weight=20 icon="\uf1b9"
-namespace cbit_小车类 {
+namespace cbit_motor {
 
     const PCA9685_ADD = 0x41
     const MODE1 = 0x00
@@ -517,12 +96,10 @@ namespace cbit_小车类 {
         OBSTACLE = 0,
         //% blockId="NOOBSTACLE" block="无障碍物"
         NOOBSTACLE = 1
-
     }
 
     
     export enum enServo {
-        
         S1 = 1,
         S2,
         S3
@@ -1039,13 +616,13 @@ namespace cbit_小车类 {
                     if (value == enLineState.White) {
                         temp = true;
                     }
-                    setPwm(7, 0, 4095);
+                    //setPwm(7, 0, 4095);
                 }
                 else {
                     if (value == enLineState.Black) {
                         temp = true;
                     }
-                    setPwm(7, 0, 0);
+                    //setPwm(7, 0, 0);
                 }
                 break;
             }
@@ -1055,13 +632,13 @@ namespace cbit_小车类 {
                     if (value == enLineState.White) {
                         temp = true;
                     }
-                    setPwm(6, 0, 4095);
+                    //setPwm(6, 0, 4095);
                 }
                 else {
                     if (value == enLineState.Black) {
                         temp = true;
                     }
-                    setPwm(6, 0, 0);
+                    //setPwm(6, 0, 0);
                 }
                 break;
             }
